e451dff1670e3592dd301ec098b2ca72f6647516
aki <64885025+aki-recapture@users.noreply.github.com>
2020-06-07 17:22:34 +0900

    Add files via upload

---
+++ MyOmikujiApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MyOmikujiApp
+//
+//  Created by 近藤明広 on 2020/06/07.
+//  Copyright © 2020 AkihiroKondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController:
+UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func getOmilluji(_ sender: Any) {
+        let results = ["大吉","中吉","末吉","大凶"]
+        let random = arc4random_uniform(UInt32(results.count))
+        self.myLabel.text = results[Int(random)]
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myLabel.layer.masksToBounds = true
+        myLabel.layer.cornerRadius = myLabel.bounds.width / 2
+    }
+
+
+}
+
