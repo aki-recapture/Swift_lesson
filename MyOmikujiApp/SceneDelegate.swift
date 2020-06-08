cf21b877d27542a7aeb06c730f125a8c47733e20
aki <64885025+aki-recapture@users.noreply.github.com>
2020-06-08 22:13:42 +0900

    Update SceneDelegate.swift

--- MyOmikujiApp/SceneDelegate.swift
+++ MyOmikujiApp/SceneDelegate.swift
@@ -1,10 +1,9 @@
-//
 //  SceneDelegate.swift
 //  MyOmikujiApp
-//
+
 //  Created by xxxxx on 2020/06/07.
 //  Copyright © 2020 xxxxx. All rights reserved.
-//
+
 
 import UIKit
 
@@ -47,7 +46,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 
 }
 
